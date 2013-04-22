5d425032fddc78db56ff49df753a4a2ff444fd54
Mukta <Mukta.Aphale@Clogeny.com>
2013-04-22 12:30:21 +0530

    knife-azure unit tests for bootstrap: changed query_azure_mock for reuse in other tests

--- spec/unit/azure_server_create_spec.rb
+++ spec/unit/azure_server_create_spec.rb
@@ -4,12 +4,14 @@
 #
 
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../unit/query_azure_mock')
 require 'chef/knife/bootstrap'
 require 'chef/knife/bootstrap_windows_winrm'
 require 'chef/knife/bootstrap_windows_ssh'
 
 describe Chef::Knife::AzureServerCreate do
 include AzureSpecHelper
+include QueryAzureMock
 
 before do    
 	@server_instance = Chef::Knife::AzureServerCreate.new
@@ -168,64 +170,6 @@ describe "for bootstrap protocol ssh:" do
 		end
 	end
 
-end
-
-
-
-def stub_query_azure(conn)
-	conn.stub(:query_azure) do |name, verb, body|
-      Chef::Log.info 'calling web service:' + name
-      if verb == 'get' || verb == nil
-        retval = ''
-        if name == 'images'
-          retval = Nokogiri::XML readFile('list_images.xml') 
-        elsif name == 'disks'
-          retval = Nokogiri::XML readFile('list_disks.xml') 
-        elsif name == 'hostedservices'
-          retval = Nokogiri::XML readFile('list_hosts.xml') 
-        elsif name == 'hostedservices/service001/deploymentslots/Production'
-          retval = Nokogiri::XML readFile('list_deployments_for_service001.xml')
-        elsif name == 'hostedservices/service002/deploymentslots/Production'
-          retval = Nokogiri::XML readFile('list_deployments_for_service002.xml')
-        elsif name == 'hostedservices/service003/deploymentslots/Production'
-          retval = Nokogiri::XML readFile('list_deployments_for_service003.xml')
-        elsif name == 'storageservices'
-          retval = Nokogiri::XML readFile('list_storageaccounts.xml')
-        else
-          Chef::Log.warn 'unknown get value:' + name
-        end
-        @getname = name
-        @getverb = verb
-        @getbody = body
-      elsif verb == 'post'
-        if name == 'hostedservices'
-          retval = Nokogiri::XML readFile('post_success.xml')
-          @receivedXML = body
-        elsif name == 'hostedservices/unknown_yet/deployments'
-          retval = Nokogiri::XML readFile('post_success.xml')
-          @receivedXML = body
-        elsif name == 'hostedservices/service001/deployments/deployment001/roles'
-          retval = Nokogiri::XML readFile('post_success.xml')
-          @receivedXML = body
-        elsif name =~ /hostedservices\/vm01.*\/deployments/
-          retval = Nokogiri::XML readFile('post_success.xml')
-          @receivedXML = body          
-        else
-          Chef::Log.warn 'unknown post value:' + name
-        end
-        @postname = name
-        @postverb = verb
-        @postbody = body
-      elsif verb == 'delete'
-        @deletename = name
-        @deleteverb = verb
-        @deletebody = body
-        @deletecount += 1
-      else
-        Chef::Log.warn 'unknown verb:' + verb
-      end
-      retval
-    end
 end
 
 end
