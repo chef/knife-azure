50f0b761e47e9e6554b4a85b6cd3ac8dfbebba84
Prabhu Das <prabhu.das@clogeny.com>
2013-05-09 15:26:10 +0530

    Minor test-case description change.

--- spec/unit/azure_server_create_spec.rb
+++ spec/unit/azure_server_create_spec.rb
@@ -18,7 +18,7 @@ before do
 
 	{
    		:azure_subscription_id => 'azure_subscription_id',
-		:azure_mgmt_cert => 'ManagementCertificate.pem',
+		:azure_mgmt_cert => 'AzureLinuxCert.pem',
 		:azure_host_name => 'preview.core.windows-int.net',
 		:role_name => 'vm01',
 		:service_location => 'service_location',
@@ -176,13 +176,13 @@ describe "for bootstrap protocol ssh:" do
 		      	Chef::Knife::Bootstrap.stub(:new).and_return(@bootstrap)
 			end
 
-			it "uses sudo password when ssh_user is not root" do
+			it "enables sudo password when ssh_user is not root" do
 		      	@bootstrap.should_receive(:run)
 				@server_instance.run
 				@bootstrap.config[:use_sudo_password].should == true
 			end
 
-			it "does not use sudo password when ssh_user is root" do
+			it "does not enable sudo password when ssh_user is root" do
 		      	@bootstrap.should_receive(:run)
 		      	Chef::Config[:knife][:ssh_user] = 'root'
 				@server_instance.run
