c105f06bc23154d196053b6875f22fc958db1bfd
Mukta Aphale <Mukta.Aphale@Clogeny.com>
2013-05-23 12:13:00 +0530

    Adding tests for quick & advance VM create options

--- spec/unit/azure_server_create_spec.rb
+++ spec/unit/azure_server_create_spec.rb
@@ -40,7 +40,9 @@ before do
     @server_instance.stub(:print)
 end
 
-describe "compulsory parameter test:" do
+describe "parameter test:" do
+
+	context "compulsory parameters" do
 
 		it "azure_subscription_id" do		
 			Chef::Config[:knife].delete(:azure_subscription_id)			
@@ -77,7 +79,39 @@ describe "compulsory parameter test:" do
 			@server_instance.ui.should_receive(:error)
 			expect {@server_instance.run}.to raise_error
 		end
+	end
 
+	context "server create options" do
+		before do
+			Chef::Config[:knife][:bootstrap_protocol] = 'ssh'
+			Chef::Config[:knife][:ssh_password] = 'ssh_password'
+			Chef::Config[:knife][:ssh_user] = 'ssh_user'
+			Chef::Config[:knife][:azure_dns_name] = 'vm01'
+			Chef::Config[:knife].delete(:azure_vm_name)
+			Chef::Config[:knife].delete(:azure_storage_account)
+			@bootstrap = Chef::Knife::Bootstrap.new
+	      	Chef::Knife::Bootstrap.stub(:new).and_return(@bootstrap)
+	      	@bootstrap.should_receive(:run)
+		end
+		it "quick create" do
+			@server_instance.should_receive(:is_image_windows?).at_least(:twice).and_return(false)
+			@server_instance.run
+			@server_instance.config[:azure_vm_name].should == "vm01"
+			#puts @receivedXML
+		end
+
+		it "advanced create" do
+			# set all params
+			Chef::Config[:knife][:azure_dns_name] = 'service001'
+			Chef::Config[:knife][:azure_vm_name] = 'vm01'
+			Chef::Config[:knife][:azure_storage_account] = 'ka001testeurope'
+			Chef::Config[:knife][:azure_os_disk_name] = 'os-disk'
+	      	#check if API calls are correct
+			@server_instance.connection.should_receive(:query_azure).with("storageservices", "post", "<?xml version=\"1.0\"?>\n<CreateStorageServiceInput xmlns=\"http://schemas.microsoft.com/windowsazure\">\n  <ServiceName>#{Chef::Config[:knife][:azure_storage_account]}</ServiceName>\n  <Label>a2EwMDF0ZXN0ZXVyb3Bl\n</Label>\n  <Description>Explicitly created storage service</Description>\n  <Location>#{Chef::Config[:knife][:azure_service_location]}</Location>\n</CreateStorageServiceInput>\n")
+			@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_dns_name]}/deployments/deployment001/roles", "post", "<?xml version=\"1.0\"?>\n<PersistentVMRole xmlns=\"http://schemas.microsoft.com/windowsazure\" xmlns:i=\"http://www.w3.org/2001/XMLSchema-instance\">\n  <RoleName>#{Chef::Config[:knife][:azure_vm_name]}</RoleName>\n  <OsVersion i:nil=\"true\"/>\n  <RoleType>PersistentVMRole</RoleType>\n  <ConfigurationSets>\n    <ConfigurationSet i:type=\"LinuxProvisioningConfigurationSet\">\n      <ConfigurationSetType>LinuxProvisioningConfiguration</ConfigurationSetType>\n      <HostName>#{Chef::Config[:knife][:azure_vm_name]}</HostName>\n      <UserName>#{Chef::Config[:knife][:ssh_user]}</UserName>\n      <UserPassword>#{Chef::Config[:knife][:ssh_password]}</UserPassword>\n      <DisableSshPasswordAuthentication>false</DisableSshPasswordAuthentication>\n    </ConfigurationSet>\n    <ConfigurationSet i:type=\"NetworkConfigurationSet\">\n      <ConfigurationSetType>NetworkConfiguration</ConfigurationSetType>\n      <InputEndpoints>\n        <InputEndpoint>\n          <LocalPort>22</LocalPort>\n          <Name>SSH</Name>\n          <Port>22</Port>\n          <Protocol>TCP</Protocol>\n        </InputEndpoint>\n      </InputEndpoints>\n    </ConfigurationSet>\n  </ConfigurationSets>\n  <Label>dm0wMQ==</Label>\n  <OSVirtualHardDisk>\n    <MediaLink>http://#{Chef::Config[:knife][:azure_storage_account]}.blob.core.windows.net/vhds/#{Chef::Config[:knife][:azure_os_disk_name]}.vhd</MediaLink>\n    <SourceImageName>SUSE__SUSE-Linux-Enterprise-Server-11SP2-20120521-en-us-30GB.vhd</SourceImageName>\n  </OSVirtualHardDisk>\n  <RoleSize>Small</RoleSize>\n</PersistentVMRole>\n")
+			@server_instance.run
+		end
+	end
 end
 
 describe "for bootstrap protocol winrm:" do
