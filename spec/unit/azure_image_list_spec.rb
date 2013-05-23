25dadc487bd508fe74e1a4c72bc1144d0030ba22
Prabhu Das <prabhu.das@clogeny.com>
2013-05-23 14:33:26 +0530

    Updated the spec to test --full and without --full outputs.

--- spec/unit/azure_image_list_spec.rb
+++ spec/unit/azure_image_list_spec.rb
@@ -30,14 +30,13 @@ describe "knife azure " do
   end
     
     it "should display only Name and OS columns." do
-      # $stdout.should_receive(:write).at_least(:twice).and_return(kind_of(String))
+      @server_instance.h.should_receive(:list).with(["Name", "OS", "CANONICAL__Canonical-Ubuntu-12-04-20120519-2012-05-19-en-us-30GB.vhd", "Linux", "MSFT__Windows-Server-2008-R2-SP1.11-29-2011", "Windows", "MSFT__Windows-Server-2008-R2-SP1-with-SQL-Server-2012-Eval.11-29-2011", "Windows", "MSFT__Windows-Server-8-Beta.en-us.30GB.2012-03-22", "Windows", "MSFT__Windows-Server-8-Beta.2-17-2012", "Windows", "MSFT__Windows-Server-2008-R2-SP1.en-us.30GB.2012-3-22", "Windows", "OpenLogic__OpenLogic-CentOS-62-20120509-en-us-30GB.vhd", "Linux", "SUSE__SUSE-Linux-Enterprise-Server-11SP2-20120521-en-us-30GB.vhd", "Linux", "SUSE__OpenSUSE64121-03192012-en-us-15GB.vhd", "Linux"], :uneven_columns_across, 2)
       @server_instance.run
-
     end
 
     it "--full should display Name , Category, Label and OS columns." do
       Chef::Config[:knife][:show_all_fields] = true
-      # $stdout.should_receive(:write).at_least(:twice).and_return(kind_of(String))
+      @server_instance.h.should_receive(:list).with(["Name", "Category", "Label", "OS", "CANONICAL__Canonical-Ubuntu-12-04-20120519-2012-05-19-en-us-30GB.vhd", "Canonical", "Ubuntu Server 12.04 20120519", "Linux", "MSFT__Windows-Server-2008-R2-SP1.11-29-2011", "Microsoft", "Windows Server 2008 R2 SP1, Nov 2011", "Windows", "MSFT__Windows-Server-2008-R2-SP1-with-SQL-Server-2012-Eval.11-29-2011", "Microsoft", "SQL Server 2012 Evaluation, Nov 2011", "Windows", "MSFT__Windows-Server-8-Beta.en-us.30GB.2012-03-22", "Microsoft", "Windows Server 8 Beta, Mar 2012", "Windows", "MSFT__Windows-Server-8-Beta.2-17-2012", "Microsoft", "Windows Server 8 Beta, Feb 2012", "Windows", "MSFT__Windows-Server-2008-R2-SP1.en-us.30GB.2012-3-22", "Microsoft", "Windows Server 2008 R2 SP1, Mar 2012", "Windows", "OpenLogic__OpenLogic-CentOS-62-20120509-en-us-30GB.vhd", "OpenLogic", "CentOS 6.2 provided by OpenLogic", "Linux", "SUSE__SUSE-Linux-Enterprise-Server-11SP2-20120521-en-us-30GB.vhd", "SUSE", "SUSE Linux Enterprise Server", "Linux", "SUSE__OpenSUSE64121-03192012-en-us-15GB.vhd", "SUSE", "OpenSUSE64-12.1-Beta", "Linux"], :uneven_columns_across, 4)
       @server_instance.run
     end
   end
