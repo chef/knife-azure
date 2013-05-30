2356ab1f5b11e8a8a94c810dd09d7ac3052ecb7b
Prabhu Das <prabhu.das@clogeny.com>
2013-05-30 12:45:46 +0530

    Using azure-dns-name for all variables and cleaned up error displayed when selected N or n for delete confirmation.

--- spec/unit/azure_server_delete_spec.rb
+++ spec/unit/azure_server_delete_spec.rb
@@ -33,7 +33,7 @@ before do
 end
 
 	it "server delete test" do
-		@server_instance.name_args = ['vm01']
+		@server_instance.name_args = ['role001']
 		@server_instance.ui.should_receive(:warn).twice
 		@server_instance.connection.roles.should_receive(:delete).and_call_original
 		@server_instance.run
@@ -47,9 +47,9 @@ end
 		@server_instance.run
 	end
 
-	it "dont cleanup hosted service when --preserve-hosted-service param set" do
-		@server_instance.name_args = ['vm01']
-		Chef::Config[:knife][:preserve_hosted_service] = true
+	it "dont cleanup hosted service when --preserve-azure-dns-name param set" do
+		@server_instance.name_args = ['role001']
+		Chef::Config[:knife][:preserve_azure_dns_name] = true
 		@server_instance.ui.should_receive(:warn).twice
 		@server_instance.connection.roles.should_receive(:delete).and_call_original
 		@server_instance.connection.hosts.should_not_receive(:delete)
@@ -60,13 +60,13 @@ end
 		test_hostname = 'vm002'
 		@server_instance.name_args = [test_hostname]
 
-		Chef::Config[:knife][:azure_hosted_service_name] = 'service001'
+		Chef::Config[:knife][:azure_dns_name] = 'service001'
 		Chef::Config[:knife][:preserve_os_disk] = true
 
 		@server_instance.connection.roles.should_receive(:delete).and_call_original
 
 		# test correct params are passed to azure API.
-		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_hosted_service_name]}/deployments/deployment001/roles/#{test_hostname}", "delete")
+		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_dns_name]}/deployments/deployment001/roles/#{test_hostname}", "delete")
 
 		@server_instance.run
 	end
@@ -75,15 +75,15 @@ end
 		test_hostnames = ['vm002', 'role002', 'role001']
 		@server_instance.name_args = test_hostnames
 
-		Chef::Config[:knife][:azure_hosted_service_name] = 'service001'
+		Chef::Config[:knife][:azure_dns_name] = 'service001'
 		Chef::Config[:knife][:preserve_os_disk] = true
 
 		@server_instance.connection.roles.should_receive(:delete).exactly(3).times.and_call_original
 
 		# test correct calls are made to azure API.
-		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_hosted_service_name]}/deployments/deployment001/roles/#{test_hostnames[0]}", "delete")
-		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_hosted_service_name]}/deployments/deployment001/roles/#{test_hostnames[1]}", "delete")
-		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_hosted_service_name]}/deployments/deployment001", "delete")
+		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_dns_name]}/deployments/deployment001/roles/#{test_hostnames[0]}", "delete")
+		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_dns_name]}/deployments/deployment001/roles/#{test_hostnames[1]}", "delete")
+		@server_instance.connection.should_receive(:query_azure).with("hostedservices/#{Chef::Config[:knife][:azure_dns_name]}/deployments/deployment001", "delete")
 
 		@server_instance.run
 	end
