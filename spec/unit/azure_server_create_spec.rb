9f17704ae6edb479f23d5e38c86c52ef1019fc8b
nimisha <nimisha.sharad@clogeny.com>
2015-02-06 12:47:55 +0530

    Added pending testcase for automatic certificate generation

--- spec/unit/azure_server_create_spec.rb
+++ spec/unit/azure_server_create_spec.rb
@@ -274,9 +274,9 @@ describe Chef::Knife::AzureServerCreate do
         @server_instance.run
       end
 
-##########  Uncomment the code below to test automatic certificate generation for ssl communication
-=begin
+
       it 'create with automatic certificates creation if winrm-transport=ssl' do
+        pending "with automatic certificates creation if winrm-transport=ssl"
         # set all params
         Chef::Config[:knife][:azure_dns_name] = 'service001'
         Chef::Config[:knife][:azure_vm_name] = 'vm002'
@@ -298,7 +298,6 @@ describe Chef::Knife::AzureServerCreate do
         #Delete temp directory
         FileUtils.remove_entry_secure dir
       end
-=end
     end
 
     context "when --azure-dns-name is not specified" do
