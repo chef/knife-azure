ca0ca0c8272ad788c8ae385db9e214186bfb4e67
siddheshwar-more <siddheshwar.more@clogeny.com>
2015-06-29 21:25:29 +0530

    Added changes for server create to set winrm_ssl_verify_mode when ssl transport specified

--- lib/chef/knife/azure_server_create.rb
+++ lib/chef/knife/azure_server_create.rb
@@ -740,8 +740,11 @@ class Chef
             bootstrap.config[:winrm_port] = port
             bootstrap.config[:auth_timeout] = locate_config_value(:auth_timeout)
             # Todo: we should skip cert generate in case when winrm_ssl_verify_mode=verify_none
-            bootstrap.config[:winrm_ssl_verify_mode] = locate_config_value(:winrm_ssl_verify_mode)
-
+            if locate_config_value(:winrm_transport) == "ssl" && locate_config_value(:thumbprint).nil? && locate_config_value(:winrm_ssl_verify_mode) == :verify_peer
+              bootstrap.config[:winrm_ssl_verify_mode] = :verify_none
+            else
+              bootstrap.config[:winrm_ssl_verify_mode] = locate_config_value(:winrm_ssl_verify_mode)
+            end
         elsif locate_config_value(:bootstrap_protocol) == 'ssh'
             bootstrap = Chef::Knife::BootstrapWindowsSsh.new
             bootstrap.config[:ssh_user] = locate_config_value(:ssh_user)
