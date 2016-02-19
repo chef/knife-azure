2cbc35c6bfa8d8763e7b27acd196a49d00d779af
nimisha <nimisha.sharad@clogeny.com>
2016-02-19 15:29:17 +0530

    Added specs for ARM server list command

--- spec/unit/azure_server_list_spec.rb
+++ spec/unit/azure_server_list_spec.rb
@@ -7,12 +7,13 @@ describe Chef::Knife::AzureServerList do
 
   before do
     @server_instance = create_instance(Chef::Knife::AzureServerList)
+    @arm_server_instance = create_arm_instance(Chef::Knife::AzureServerList)
+  end
+
+  it "should display DNS Name, VM Name, Status, IP Address, SSH Port and Winrm Port for ASM command." do
     Chef::Config[:knife][:azure_api_mode] = "asm"
     stub_query_azure(@server_instance.service.connection)
     allow(@server_instance).to receive(:puts)
-  end
-
-  it "should display DNS Name, VM Name, Status, IP Address, SSH Port and Winrm Port." do
     expect(@server_instance.ui).to receive(:list).
       with(["DNS Name", "VM Name", "Status", "IP Address", "SSH Port", "WinRM Port",
         "service001.cloudapp.net", "vm002", "ready", "65.52.251.57", "22", "",
@@ -23,5 +24,14 @@ describe Chef::Knife::AzureServerList do
         "service004.cloudapp.net", "winrm-vm", "ready", "65.52.249.191", "", "5985",
         "vmname.cloudapp.net", "vmname", "ready", "65.52.251.57", "22", ""], :uneven_columns_across, 6)
     @server_instance.run
+  end
+
+  it "should display VM Name, Location, Provisioning State and OS Type for ARM command." do
+    Chef::Config[:knife][:azure_api_mode] = "arm"
+    compute_mgmt_client = double("ComputeManagementClient")
+    expect(@arm_server_instance.service).to receive(:compute_management_client).and_return(compute_mgmt_client)
+    expect(compute_mgmt_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([])
+    expect(@arm_server_instance.service).to receive(:display_list).with(@arm_server_instance.service.ui, ["VM Name", "Location", "Provisioning State", "OS Type"],[])
+    @arm_server_instance.run
   end
 end
