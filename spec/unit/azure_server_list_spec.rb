8c55f8eec582655371112d70a95d2ba10667c1d1
aliasgar16 <aliasgar.batterywala@clogeny.com>
2016-02-25 14:22:24 +0530

    Modified ARM server_list RSpecs.

--- spec/unit/azure_server_list_spec.rb
+++ spec/unit/azure_server_list_spec.rb
@@ -28,9 +28,9 @@ describe Chef::Knife::AzureServerList do
 
   it "should display VM Name, Location, Provisioning State and OS Type for ARM command." do
     Chef::Config[:knife][:azure_api_mode] = "arm"
-    compute_mgmt_client = double("ComputeManagementClient")
-    expect(@arm_server_instance.service).to receive(:compute_management_client).and_return(compute_mgmt_client)
-    expect(compute_mgmt_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([])
+    compute_client = double("ComputeClient")
+    expect(@arm_server_instance.service).to receive(:get_compute_client).and_return(compute_client)
+    expect(compute_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([])
     expect(@arm_server_instance.service).to receive(:display_list).with(@arm_server_instance.service.ui, ["VM Name", "Location", "Provisioning State", "OS Type"],[])
     @arm_server_instance.run
   end
