46cc7b85249ead992ed0209f8b62651c46f24b38
nimisha <nimisha.sharad@clogeny.com>
2016-03-01 15:13:19 +0530

    Added specs with mock server object

--- spec/unit/azurerm_server_list_spec.rb
+++ spec/unit/azurerm_server_list_spec.rb
@@ -7,13 +7,26 @@ describe Chef::Knife::AzurermServerList do
 
   before do
     @arm_server_instance = create_arm_instance(Chef::Knife::AzurermServerList)
+    @compute_client = double("ComputeManagementClient")
+
+    @server = double("server", :name => "MyVM", :location => "west-us", :properties => double)
+    allow(@server.properties).to receive(:provisioning_state).and_return("running")
+    allow(@server.properties).to receive_message_chain(:storage_profile, :os_disk, :os_type).and_return("linux")
   end
 
-  it "should display VM Name, Location, Provisioning State and OS Type for ARM command." do
-    compute_client = double("ComputeManagementClient")
-    expect(@arm_server_instance.service).to receive(:compute_management_client).and_return(compute_client)
-    expect(compute_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([])
+  it "should display only labels if there are no servers" do
+    expect(@arm_server_instance.service).to receive(:compute_management_client).and_return(@compute_client)
+    expect(@compute_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([])
     expect(@arm_server_instance.service).to receive(:display_list).with(@arm_server_instance.service.ui, ["VM Name", "Location", "Provisioning State", "OS Type"],[])
+    @arm_server_instance.run
+  end
+
+  it "should display VM Name, Location, Provisioning State and OS Type" do
+    expect(@arm_server_instance.service).to receive(:compute_management_client).and_return(@compute_client)
+    expect(@compute_client).to receive_message_chain(:virtual_machines, :list_all, :value!, :body, :value).and_return([@server])
+    #output_row is based on the @server double object
+    output_row = [@server.name, @server.location, "ready", @server.properties.storage_profile.os_disk.os_type]
+    expect(@arm_server_instance.service).to receive(:display_list).with(@arm_server_instance.service.ui, ["VM Name", "Location", "Provisioning State", "OS Type"], output_row)
     @arm_server_instance.run
   end
 end
