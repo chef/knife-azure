d5768a5122d9b0b0ef17e461949e832341378da9
Vasu1105 <vasundhara.jagdale@clogeny.com>
2016-06-28 17:19:08 +0530

    Added specs for find_file

--- spec/unit/azure_base_spec.rb
+++ spec/unit/azure_base_spec.rb
@@ -224,5 +224,12 @@ describe Chef::Knife::AzureBase do
 	  	end
 	  end
 
+	  context "find_file" do
+      it "finds the file with given path" do
+        file_path = get_publish_settings_file_path("azureValid.publishsettings")
+        Chef::Config[:knife][:azure_publish_settings_file] = get_publish_settings_file_path("azureValid.publishsettings")
+        expect(@dummy.find_file(Chef::Config[:knife][:azure_publish_settings_file])).to eq file_path
+      end
+    end
 	end
 end
