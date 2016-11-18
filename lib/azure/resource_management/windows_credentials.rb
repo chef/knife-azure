16b3eeb6885667a5ebf266f1edf33ee6da642b3a
nimisha <nimisha.sharad@msystechnologies.com>
2016-11-18 14:55:08 +0530

    Added specs
    Signed-off-by: nimisha <nimisha.sharad@msystechnologies.com>

--- lib/azure/resource_management/windows_credentials.rb
+++ lib/azure/resource_management/windows_credentials.rb
@@ -126,9 +126,9 @@ module Azure::ARM
         # cmdkey command is used for accessing windows credential manager.
         # Multiple credentials get created in windows credential manager for a single Azure account in xplat-cli
         # One of them is for common tanent id, which can't be used
-        # Two of them end with --0-2 and --1-2. The one ending with --0-2 has the complete
-        # accessToken in the credentialBlob.
-        # Refresh Token is split across both credentials (ending with --0-2 and --1-2).
+        # Others end with --0-x,--1-x,--2-x etc, where x represents the total no. of credentails across which the token is divided
+        # The one ending with --0-x has the complete accessToken in the credentialBlob.
+        # Refresh Token is split across both credentials (ending with --0-x and --1-x).
         # Xplat splits the credentials based on the number of bytes of the tokens.
         # Hence the access token is always found in the one which start with --0-
         # So selecting the credential on the basis of --0-
@@ -137,6 +137,7 @@ module Azure::ARM
         target_names = []
 
         if result.stdout.empty?
+          Chef::Log.debug("Unable to find a credential with --0- and falling back to looking for any credential.")
           xplat_creds_cmd = Mixlib::ShellOut.new('cmdkey /list | findstr AzureXplatCli | findstr -v common')
           result = xplat_creds_cmd.run_command
 
