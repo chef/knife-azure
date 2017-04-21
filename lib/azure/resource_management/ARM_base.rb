281894ebf18b005b264b3103a1b104310e84774d
Simon Kaluza <kaluza.simon@gmail.com>
2017-04-21 17:50:08 -0400

    Update size_hash to allow non-standard VM sizes
    Currently ARMBase restricts allowed VM sizes to an arbitrary set of default sizes.  If a users specifies a custom but valid raw size, (e.g. "Standard_DS3_v2") as their parameter, there's no reason we can't at least try to pass that through directly.

--- lib/azure/resource_management/ARM_base.rb
+++ lib/azure/resource_management/ARM_base.rb
@@ -23,7 +23,7 @@ module Azure::ARM
         size_hash = { "ExtraSmall" => "Standard_A0", "Small" => "Standard_A1",
                       "Medium" => "Standard_A2", "Large" => "Standard_A3",
                       "ExtraLarge" => "Standard_A4" }
-        size_hash[size_name]
+        size_hash[size_name].nil? ? size_name : size_hash[size_name]
       end
     end
 end
