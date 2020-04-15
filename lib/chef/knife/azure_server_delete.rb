57891bfc21c33066a1fd12511149b650dda70563
Lamont Granquist <lamont@scriptkiddie.org>
2020-04-14 19:17:33 -0700

    nuke all the copyright dates
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- lib/chef/knife/azure_server_delete.rb
+++ lib/chef/knife/azure_server_delete.rb
@@ -2,7 +2,7 @@
 # Author:: Barry Davis (barryd@jetstreamsoftware.com)
 # Author:: Adam Jacob (<adam@chef.io>)
 # Author:: Seth Chisamore (<schisamo@chef.io>)
-# Copyright:: Copyright 2010-2020, Chef Software Inc.
+# Copyright:: Copyright (c) Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- lib/chef/knife/azure_server_show.rb
+++ lib/chef/knife/azure_server_show.rb
@@ -2,7 +2,7 @@
 # Author:: Barry Davis (barryd@jetstreamsoftware.com)
 # Author:: Seth Chisamore (<schisamo@chef.io>)
 # Author:: Adam Jacob (<adam@chef.io>)
-# Copyright:: Copyright 2010-2019, Chef Software Inc.
+# Copyright:: Copyright (c) Chef Software Inc.
 # License:: Apache License, Version 2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
